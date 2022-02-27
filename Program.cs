82a04f8fca7788c3ada2537fcd37109c0c1de7ec
GordonV03 <92227408+GordonV03@users.noreply.github.com>
2022-02-27 18:42:25 +0500

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,67 +1,33 @@
 ﻿using System;
 using System.Collections.Generic;
 
-/*Гордон Вячеслав*/
-
 namespace ConsoleApp2
 {
     class Program
     {
         static void Main(string[] args)
         {
-
+            
         }
     }
 
     class Queue<T>
         where T: IComparable<T>
     {
-        T max;
-        LinkedList<Tuple<T,T>> queue = new LinkedList<Tuple<T,T>>();
-
-        public int Compare(T other)
-        {
-            if (max.CompareTo(other) > 0)
-            {
-                return 1;
-            }
-            else
-            if (max.CompareTo(other) < 0)
-            {
-                return -1;
-            }
-            else
-            {
-                return 0;
-            }
-        }
+        LinkedList<T> stack1 = new LinkedList<T>();
+        LinkedList<T> stack2 = new LinkedList<T>();
 
         public void Push(T item)
         {
-            if (queue.Count == 0)
-                queue.AddLast(Tuple.Create(item, item));
-            else
-                if (Compare(item) == -1)
-                  queue.AddLast(Tuple.Create(item, max));
-                else
-                    queue.AddLast(Tuple.Create(item, queue.Last.Value.Item2));
-            max = queue.Last.Value.Item2;
+            stack1.AddLast(item);
         }
 
         public T Pop()
         {
-            if (queue.Count == 0) throw new InvalidOperationException();
-            var result = queue.Last.Value;
-            queue.RemoveLast();
-            return result.Item1;
-        }
-
-        public T Max
-        {
-            get
-            {
-                return queue.Last.Value.Item2;
-            }
+            if (stack2.Count == 0)
+                while (stack1.Count != 0)
+                    stack2.AddLast(stack1.Last);
+            return stack2.Last.Value;
         }
     }
 }
