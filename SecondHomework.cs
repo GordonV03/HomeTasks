e1a7743d219e339bbda069738ffc3466d9f657f9
GordonV03 <92227408+GordonV03@users.noreply.github.com>
2022-02-27 15:27:41 +0500

    Add files via upload

---
+++ SecondHomework.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+
+    class Queue<T>
+        where T: IComparable<T>
+    {
+        LinkedList<T> stack1 = new LinkedList<T>();
+        LinkedList<T> stack2 = new LinkedList<T>();
+
+        public void Push(T item)
+        {
+            stack1.AddLast(item);
+        }
+
+        public T Pop()
+        {
+            if (stack2.Count == 0)
+                while (stack1.Count != 0)
+                    stack2.AddLast(stack1.Last);
+            return stack2.Last.Value;
+        }
+    }
+}
